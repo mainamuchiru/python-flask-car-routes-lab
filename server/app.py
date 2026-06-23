ce22283b456b4348dba868511133add64ddc114d
mainamuchiru <mainaephantus@gmail.com>
2026-06-23 21:25:56 +0300

    Routing feature added

--- server/app.py
+++ server/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route('/')
+def index():
+    return 'Welcome to Flatiron Cars'
+
+@app.route('/<string:car_name>')
+def car(car_name):
+    if car_name in existing_models:
+        return f'Flatiron {car_name} is in our fleet!'
+    else:
+        return f'No models called {car_name} exists in our catalog'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+
